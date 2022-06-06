38b4e0e9f3bdb1ac7ed8e9105494e4258c732a67
Mutluaydin <mutlumelikaydin1@gmail.com>
2022-06-06 19:09:52 +0200

    sayi tahmini

---
+++ SayiTahminOyunu.py
@@ -0,0 +1,31 @@
+import random
+
+'''
+   1-100 arasında rastgele üretilecek bir sayıyı aşağı yukarı ifadeleri ile
+   buldurmaya çalışın. (hak = 5)
+   ** "random modülü" için "python random" şeklinde arama yapın.
+   ** 100 üzerinden puanlama yapın. Her soru 20 puan.
+   ** Hak bilgisini kullanıcıdan alın ve her soru belirtilen can sayısı
+      üzerinden hesaplansın.
+'''
+sayi = random.randint(1,10)
+can = int(input("can sayisini girin: "))
+hak = can
+sayac = 0
+
+while hak > 0:
+    hak -= 1
+    sayac += 1
+    tahmin = int(input("bir sayi giriniz: "))
+
+    if tahmin == sayi:
+        print(f"tebrikler bildiniz. Toplam puaniniz: {100 - (100 / can) * (sayac - 1)}" )
+        break
+    elif sayi > tahmin:
+        print("Yukari")
+    else:
+        print("Asagi")
+
+    if hak == 0:
+        print(f"Hakkiniz bitti. Tutulan sayi: {sayi} ")
+
